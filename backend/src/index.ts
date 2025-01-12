6f3070bb2b10ad714788decb0eef8736972e08df
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2025-01-12 19:12:04 +0530

    Update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,7 @@ app.use(
       origin: [
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://youtube-video-summarizer-rose-six.vercel.app/"
         // "https://ecox.somnathcodes.site",
         // "https://ecox.wedevelopers.online",
       ],
